042d49fffff547ac840aa16768b56fe5b5505acb
Eugene Evstratev <levipupsik321@gmail.com>
2025-10-19 18:26:22 +0300

    Создал две модели, Product связана с Category по FK, дата создания и изменения задаются автоматически, описал класс Meta, дандер метод __str__.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    # create_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        db_table = 'categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(upload_to='media/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, to_field='id', on_delete=models.CASCADE, verbose_name='Категория')
+    price_per_piece = models.IntegerField(verbose_name='Цена за штуку')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        db_table = 'products'
